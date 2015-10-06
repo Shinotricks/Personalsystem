9587240d649668a28dee10018718779e83a2750d
Minatanae <christoffer@mierei.com>
2015-10-06 13:10:45 +0200

    Added JobController and Todolist

---
+++ Personalsystem/Controllers/JobController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Personalsystem.Models;
+using Personalsystem.Repositories;
+
+namespace Personalsystem.Controllers
+{
+    public class JobController : Controller
+    {
+        private PersonalsystemRepository repo = new PersonalsystemRepository();
+        
+        // GET: Companies
+        public ActionResult Index()
+        {
+            return View(repo.Jobs());
+        }
+
+        // GET: Companies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = repo.GetSpecificCompany(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.CreateCompany(company);
+                return RedirectToAction("Index");
+            }
+
+            return View(company);
+        }
+
+        // GET: Companies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = repo.GetSpecificCompany(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.EditCompany(company);
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+
+        // GET: Companies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Company company = repo.GetSpecificCompany(id);
+            if (company == null)
+            {
+                return HttpNotFound();
+            }
+            return View(company);
+        }
+
+        // POST: Companies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Company company = repo.GetSpecificCompany(id);
+            
+            repo.RemoveCompany(company);
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Personalsystem/TODOLIST/Todolist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Personalsystem.TODOLIST
+{
+    public class Todolist
+    {
+    }
+}
+
+/* 
+Durim
+Lägga till arbetstid till arbetare.
+Skapa viewmodel till admin.
+Skapa viewmodel till chef.
+Skapa en viewmodel för att visa företaget.
+
+Peter
+Skapa en funktion som lägger till ett företags ledning.
+Skapa en funktion som lägger till ett företags avdelningar.
+Skapa en funktion som lägger till en avdelnings grupper.
+Skapa en funktion som lägger till (bjuder in) personal i företaget (admin).
+
+Aron
+Som admin skall jag kunna ändra roll på andra användare(kopplade till företaget) till chef eller arbetare.
+Som admin skall jag kunna vara owner och kunna lägga till fler admin.
+Skapa en funktion för att automatiskt läggas till i gruppen sökande när jag skapar mitt konto.
+Som admin, chef eller arbetare skall jag kunna återgå till sökande genom att tas bort från företaget.
+
+Christoffer
+Skapa en vy som visar olika ansökningar som ligger under ett ansvarsområde.
+Skapa en vy för att visa ansökningar.
+Som admin och chef skall jag kunna lägga ut nyheter.
+Som admin skall jag kunna bestämma vilka nyheter som ska vara interna eller externa.
+ */
