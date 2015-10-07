148df0d569887cb0400d31c187e66edb37c521a9
Shinotricks <Shinotrix@gmail.com>
2015-10-07 14:04:39 +0200

    Pulla detta!

--- Personalsystem/Controllers/GroupController.cs
+++ Personalsystem/Controllers/GroupController.cs
@@ -18,7 +18,11 @@ namespace Personalsystem.Controllers
         // GET: Groups
         public ActionResult Index(int? id)
         {
-            //var groups = repo.Groups.Include(g => g.Department);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ViewBag.Group = repo.GetSpecificGroup(id).Name;
             return View(repo.UserViewModelsByGroupId(id));
         }
 
