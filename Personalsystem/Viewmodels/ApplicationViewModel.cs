5b7d4940c81c23c7bda1709ba0e29679cfe2c322
Minatanae <christoffer@mierei.com>
2015-10-22 15:12:48 +0200

    TROLOLO

---
+++ Personalsystem/Viewmodels/ApplicationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Personalsystem.Models;
+using System.Web.Mvc;
+
+namespace Personalsystem.Viewmodels
+{
+    public class ApplyForJobViewModel
+    {
+        public string Applicant { get; set; }
+        public DateTime ApplicationDate { get; set; }
+        public Job Job { get; set; }
+
+    }
+}
